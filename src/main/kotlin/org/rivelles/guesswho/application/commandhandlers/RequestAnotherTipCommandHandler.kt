fde6573f3af30329d06abfbdb386434cb192cc28
Lucas Rivelles <lucas.rivelles@glovoapp.com>
2023-01-03 22:00:40 +0100

    Spotless

--- src/main/kotlin/org/rivelles/guesswho/application/commandhandlers/RequestAnotherTipCommandHandler.kt
+++ src/main/kotlin/org/rivelles/guesswho/application/commandhandlers/RequestAnotherTipCommandHandler.kt
@@ -7,11 +7,13 @@ import org.rivelles.guesswho.domain.repositories.SessionRepository
 class RequestAnotherTipCommandHandler(private val sessionRepository: SessionRepository) :
     CommandHandler<RequestAnotherTipCommand> {
     override fun handle(command: RequestAnotherTipCommand) {
-        sessionRepository.findTodaySessionForUser(command.userIdentifier)?.let {
-            session -> session.requestOneMoreTip()
-            session
-        }?.run {
-            sessionRepository.save(this)
-        } ?: throw RuntimeException("Session not found for user")
+        sessionRepository
+            .findTodaySessionForUser(command.userIdentifier)
+            ?.let { session ->
+                session.requestOneMoreTip()
+                session
+            }
+            ?.run { sessionRepository.save(this) }
+            ?: throw RuntimeException("Session not found for user")
     }
 }
