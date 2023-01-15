7356d1ee14a75161f0146baea255771260aee572
Lucas Rivelles <lucas.rivelles@glovoapp.com>
2023-01-15 20:39:08 +0100

    Make IT run

--- application/src/test/kotlin/adapters/http/SessionsRouterIT.kt
+++ application/src/test/kotlin/adapters/http/SessionsRouterIT.kt
@@ -1,43 +1,45 @@
-package adapters.http
+package org.rivelles.adapters.http
 
-import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldNotBe
-import io.kotest.spring.SpringListener
-import org.rivelles.GuessWhoApplication
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.extensions.spring.SpringExtension
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
-import org.springframework.context.ApplicationContext
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.TestPropertySource
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.testcontainers.containers.PostgreSQLContainer
 
-@ContextConfiguration(classes = [(GuessWhoApplication::class)])
-@TestPropertySource("classpath:application.properties")
-@WebFluxTest
-class SessionsRouterIT : FunSpec() {
-    override fun listeners() = listOf(SpringListener)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class SessionsRouterIT : StringSpec() {
 
-    @Autowired lateinit var applicationContext: ApplicationContext
+    override fun extensions() = listOf(SpringExtension)
 
-    lateinit var client: WebTestClient
+    @Autowired lateinit var webTestClient: WebTestClient
 
     private val postgreSQLContainer: PostgreSQLContainer<*> =
         PostgreSQLContainer("postgres:11.1")
             .withDatabaseName("integration-tests-db")
             .withUsername("user")
-            .withPassword("user")
+            .withPassword("password")
 
     init {
         postgreSQLContainer.start()
-        System.setProperty("spring.r2dbc.url", postgreSQLContainer.host)
-        System.setProperty("spring.r2dbc.username", postgreSQLContainer.username)
-        System.setProperty("spring.r2dbc.password", postgreSQLContainer.password)
 
-        beforeAny { client = WebTestClient.bindToApplicationContext(applicationContext).build() }
-        test("Should run") { applicationContext shouldNotBe null }
-        test("When creating session with empty body, should receive client error") {
-            client.post().uri("/sessions").exchange().expectStatus().is4xxClientError
+        "Should create session" {
+            val requestBody = CreateSessionForUserRequest("127.0.0.1")
+
+            webTestClient
+                .post()
+                .uri("/sessions")
+                .contentType(APPLICATION_JSON)
+                .bodyValue(requestBody)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful
+        }
+        "When creating session with empty body, should receive client error" {
+            webTestClient.post().uri("/sessions").exchange().expectStatus().is4xxClientError
         }
     }
 }
