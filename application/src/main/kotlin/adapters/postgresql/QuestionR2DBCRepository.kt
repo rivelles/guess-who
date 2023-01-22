ea02998b5f0c15d5ffc83e675ff0463401b27228
Lucas Rivelles <lucas.rivelles@glovoapp.com>
2023-01-22 15:09:16 +0100

    Improves repositories by using coroutines

--- application/src/main/kotlin/adapters/postgresql/QuestionR2DBCRepository.kt
+++ application/src/main/kotlin/adapters/postgresql/QuestionR2DBCRepository.kt
@@ -9,7 +9,11 @@ import QuestionImage
 import QuestionTips
 import java.time.LocalDate
 import java.util.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import kotlinx.coroutines.runBlocking
 import org.springframework.r2dbc.core.DatabaseClient
+import org.springframework.r2dbc.core.awaitRowsUpdated
 import org.springframework.stereotype.Repository
 import repositories.QuestionRepository
 
@@ -17,80 +21,82 @@ import repositories.QuestionRepository
 class QuestionR2DBCRepository(val databaseClient: DatabaseClient) : QuestionRepository {
     override fun getQuestionOfTheDay(): Question? {
         val today = LocalDate.now()
-        return databaseClient
-            .sql {
-                """
+        return runBlocking {
+            databaseClient
+                .sql {
+                    """
             SELECT * FROM questions q
             INNER JOIN question_tips qt ON qt.question_id = q.id
             WHERE date_appearance = :date_appearance
         """.trimIndent()
-            }
-            .bind("date_appearance", today)
-            .fetch()
-            .all()
-            .map { rows ->
-                val tips = rows["tip"].toString()
-                val questionId = UUID.fromString(rows["question_id"].toString())
-                val description = rows["description"].toString()
-                val answer = rows["answer"].toString()
-                val image = rows["image"].toString()
-                val dateOfAppearance =
-                    LocalDate.parse(rows["date_appearance"].toString().split("T")[0])
+                }
+                .bind("date_appearance", today)
+                .fetch()
+                .all()
+                .map { rows ->
+                    val tips = rows["tip"].toString()
+                    val questionId = UUID.fromString(rows["question_id"].toString())
+                    val description = rows["description"].toString()
+                    val answer = rows["answer"].toString()
+                    val image = rows["image"].toString()
+                    val dateOfAppearance =
+                        LocalDate.parse(rows["date_appearance"].toString().split("T")[0])
 
-                Question(
-                    QuestionId(questionId),
-                    QuestionDescription(description),
-                    QuestionAnswer(answer),
-                    QuestionTips(listOf(tips)),
-                    QuestionImage(image),
-                    QuestionDateOfAppearance(dateOfAppearance))
-            }
-            .reduce { q1, q2 ->
-                val questionTips =
-                    QuestionTips(listOf(q1.questionTips.tips[0], q2.questionTips.tips[0]))
-                q1.copy(questionTips = questionTips)
-            }
-            .toFuture()
-            .get()
+                    Question(
+                        QuestionId(questionId),
+                        QuestionDescription(description),
+                        QuestionAnswer(answer),
+                        QuestionTips(listOf(tips)),
+                        QuestionImage(image),
+                        QuestionDateOfAppearance(dateOfAppearance))
+                }
+                .reduce { q1, q2 ->
+                    val questionTips =
+                        QuestionTips(listOf(q1.questionTips.tips[0], q2.questionTips.tips[0]))
+                    q1.copy(questionTips = questionTips)
+                }
+                .awaitSingleOrNull()
+        }
     }
 
     override fun save(question: Question) {
-        databaseClient
-            .sql {
-                """
+        runBlocking {
+            val questionCreated =
+                databaseClient
+                    .sql {
+                        """
             INSERT INTO questions
             (id, description, answer, image, date_appearance)
             VALUES
             (:id, :description, :answer, :image, :date_appearance)
         """.trimIndent()
-            }
-            .bind("id", question.questionId.id)
-            .bind("description", question.questionDescription.description)
-            .bind("answer", question.questionAnswer.answer)
-            .bind("image", question.questionImage.imageUrl)
-            .bind("date_appearance", question.questionDateOfAppearance.dateOfAppearance)
-            .fetch()
-            .one()
-            .toFuture()
-            .get()
+                    }
+                    .bind("id", question.questionId.id)
+                    .bind("description", question.questionDescription.description)
+                    .bind("answer", question.questionAnswer.answer)
+                    .bind("image", question.questionImage.imageUrl)
+                    .bind("date_appearance", question.questionDateOfAppearance.dateOfAppearance)
+                    .fetch()
+                    .awaitRowsUpdated()
 
-        // TODO: Improve bulk insert
-        question.questionTips.tips.forEach {
-            databaseClient
-                .sql {
-                    """
+            if (questionCreated < 1) throw RuntimeException("Couldn't create question")
+            question.questionTips.tips.forEach {
+                launch {
+                    databaseClient
+                        .sql {
+                            """
             INSERT INTO question_tips
             (id, question_id, tip)
             VALUES
             (:id, '${question.questionId.id}', :tip)
         """.trimIndent()
+                        }
+                        .bind("id", UUID.randomUUID().toString())
+                        .bind("tip", it)
+                        .fetch()
+                        .awaitRowsUpdated()
                 }
-                .bind("id", UUID.randomUUID().toString())
-                .bind("tip", it)
-                .fetch()
-                .one()
-                .toFuture()
-                .get()
+            }
         }
     }
 }
--- application/src/main/kotlin/adapters/postgresql/SessionR2DBCRepository.kt
+++ application/src/main/kotlin/adapters/postgresql/SessionR2DBCRepository.kt
@@ -12,7 +12,10 @@ import SessionDate
 import UserIdentifier
 import java.time.LocalDate
 import java.util.UUID
+import kotlinx.coroutines.runBlocking
 import org.springframework.r2dbc.core.DatabaseClient
+import org.springframework.r2dbc.core.awaitOneOrNull
+import org.springframework.r2dbc.core.awaitRowsUpdated
 import org.springframework.stereotype.Repository
 import repositories.SessionRepository
 
@@ -20,9 +23,10 @@ import repositories.SessionRepository
 class SessionR2DBCRepository(val databaseClient: DatabaseClient) : SessionRepository {
     override fun findTodaySessionForUser(userIdentifier: UserIdentifier): Session? {
         val today = LocalDate.now()
-        return databaseClient
-            .sql {
-                """
+        return runBlocking {
+            databaseClient
+                .sql {
+                    """
             SELECT * FROM sessions s 
             INNER JOIN questions q ON s.question_id = q.id 
             INNER JOIN question_tips qt ON q.id = qt.question_id 
@@ -30,75 +34,75 @@ class SessionR2DBCRepository(val databaseClient: DatabaseClient) : SessionReposi
             WHERE s.user_identifier = :user_identifier  
             AND s.session_started_date = :session_started_date
         """.trimIndent()
-            }
-            .bind("user_identifier", userIdentifier.userIp)
-            .bind("session_started_date", today)
-            .map { row, _ ->
-                val questionTips =
-                    QuestionTips(
-                        row.get("qt.tip", ArrayList<String>().javaClass)
-                            ?: throw RuntimeException(
-                                "Couldn't find question tips for user session"))
-                val showedTips =
-                    QuestionTips(
-                        row.get("qt.tip", ArrayList<String>().javaClass)
-                            ?: throw RuntimeException("Couldn't find showed tips for user session"))
-                val question =
-                    Question(
-                        questionId =
-                            QuestionId(
-                                row.get("q.id", UUID::class.java)
-                                    ?: throw RuntimeException(
-                                        "Couldn't find question ID for user session")),
-                        questionDescription =
-                            QuestionDescription(
-                                row.get("q.description", String::class.java)
-                                    ?: throw RuntimeException(
-                                        "Couldn't find question description for user session")),
-                        questionAnswer =
-                            QuestionAnswer(
-                                row.get("q.answer", String::class.java)
-                                    ?: throw RuntimeException(
-                                        "Couldn't find question answer for user session")),
-                        questionTips = questionTips,
-                        questionImage =
-                            QuestionImage(
-                                row.get("q.image", String::class.java)
-                                    ?: throw RuntimeException(
-                                        "Couldn't find question image for user session")),
-                        questionDateOfAppearance =
-                            QuestionDateOfAppearance(
-                                row.get("q.date_appearance", LocalDate::class.java)
-                                    ?: throw RuntimeException(
-                                        "Couldn't find question date of appearance for user session")),
-                    )
-                Session(
-                    userIdentifier = userIdentifier,
-                    sessionStartedDate = SessionDate(LocalDate.now()),
-                    sessionFinishedDate = null,
-                    question = question,
-                    showedTips = showedTips)
-            }
-            .first()
-            .toFuture()
-            .get()
+                }
+                .bind("user_identifier", userIdentifier.userIp)
+                .bind("session_started_date", today)
+                .map { row, _ ->
+                    val questionTips =
+                        QuestionTips(
+                            row.get("qt.tip", ArrayList<String>().javaClass)
+                                ?: throw RuntimeException(
+                                    "Couldn't find question tips for user session"))
+                    val showedTips =
+                        QuestionTips(
+                            row.get("qt.tip", ArrayList<String>().javaClass)
+                                ?: throw RuntimeException(
+                                    "Couldn't find showed tips for user session"))
+                    val question =
+                        Question(
+                            questionId =
+                                QuestionId(
+                                    row.get("q.id", UUID::class.java)
+                                        ?: throw RuntimeException(
+                                            "Couldn't find question ID for user session")),
+                            questionDescription =
+                                QuestionDescription(
+                                    row.get("q.description", String::class.java)
+                                        ?: throw RuntimeException(
+                                            "Couldn't find question description for user session")),
+                            questionAnswer =
+                                QuestionAnswer(
+                                    row.get("q.answer", String::class.java)
+                                        ?: throw RuntimeException(
+                                            "Couldn't find question answer for user session")),
+                            questionTips = questionTips,
+                            questionImage =
+                                QuestionImage(
+                                    row.get("q.image", String::class.java)
+                                        ?: throw RuntimeException(
+                                            "Couldn't find question image for user session")),
+                            questionDateOfAppearance =
+                                QuestionDateOfAppearance(
+                                    row.get("q.date_appearance", LocalDate::class.java)
+                                        ?: throw RuntimeException(
+                                            "Couldn't find question date of appearance for user session")),
+                        )
+                    Session(
+                        userIdentifier = userIdentifier,
+                        sessionStartedDate = SessionDate(LocalDate.now()),
+                        sessionFinishedDate = null,
+                        question = question,
+                        showedTips = showedTips)
+                }
+                .awaitOneOrNull()
+        }
     }
 
     override fun save(session: Session) {
-        databaseClient
-            .sql {
-                """
+        runBlocking {
+            databaseClient
+                .sql {
+                    """
             INSERT INTO sessions (id, user_identifier, session_started_date, question_id) 
             VALUES (:id, :user_identifier, :session_started_date, :question_id)
         """.trimIndent()
-            }
-            .bind("id", UUID.randomUUID().toString())
-            .bind("user_identifier", session.userIdentifier.userIp)
-            .bind("session_started_date", session.sessionStartedDate.date)
-            .bind("question_id", session.question.questionId.id.toString())
-            .fetch()
-            .one()
-            .toFuture()
-            .get()
+                }
+                .bind("id", UUID.randomUUID().toString())
+                .bind("user_identifier", session.userIdentifier.userIp)
+                .bind("session_started_date", session.sessionStartedDate.date)
+                .bind("question_id", session.question.questionId.id.toString())
+                .fetch()
+                .awaitRowsUpdated()
+        }
     }
 }
