1abfce642b13c22e7f90ab07614e007e87ff7255
Lucas Rivelles <lucas.rivelles@glovoapp.com>
2023-09-03 18:47:17 +0200

    chore: Create docker-compose

--- guess-who-adapters/build.gradle.kts
+++ guess-who-adapters/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     testImplementation("io.mockk:mockk:1.13.2")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
     testImplementation("org.testcontainers:postgresql:1.17.6")
-    testImplementation("org.postgresql:postgresql:42.5.1")
+    runtimeOnly("org.postgresql:postgresql:42.5.1")
     testImplementation("io.projectreactor:reactor-test:3.5.2")
     testImplementation(testFixtures(project(":guess-who-domain")))
 }
